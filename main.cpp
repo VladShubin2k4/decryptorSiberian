2b6c227d0a356dc9249b2b5170efa857dfe82760
Vlad <VladShubin2k4@gmail.com>
2021-03-09 15:11:11 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,55 +4,63 @@
 #include <string>
 using namespace std;
 
-int main(){
-    ios::sync_with_stdio(false);
-    SetConsoleCP(1251);SetConsoleOutputCP(1251);
-
-    const short R=32,C=32;
-    char table[R][C]={};
-
-    string str; getline(cin,str);
+void DeleteExtraSPACE(string& str){
     for(short i=4; i<str.length();){
         if(str[i]==' '){
             str.erase(i,1);
             i+=4;
         }
     }
+}
 
-    short cnt=0,i=0,j=0,len=str.length();
-    while(cnt++<=len){
-        if(i==0){
-            if(cnt==1) table[i][j++]='1';
-            else{--cnt; ++j;}
-            if(++cnt<=len) table[i][j]='1';
-        }
+void BuildDoll(char (&table)[32][32], short& len){
+    short cnt=0,i=0,j=0;
+    table[i][j]='1';
+    while(++cnt<=len){
+        if(i==0) table[i][++j]='1';
         while(j>0 && ++cnt<=len) table[++i][--j]='1';
         if(j==0 && ++cnt<=len) table[++i][j]='1';
-        while(i!=0 && ++cnt<=len)  table[--i][++j]='1';
+        while(i>0 && ++cnt<=len)  table[--i][++j]='1';
     }
-    cnt=0;
+}
+
+void FillDoll(char (&table)[32][32], short& len, string& str){
+    short cnt=0;
     while(cnt<=len){
-        for(short it=0; cnt<=len; ++it){
-            for(short jt=0; table[it][jt]!='\0' && cnt<=len; ++jt) table[it][jt]=str[cnt++];
+        for(short i=0; cnt<=len; ++i){
+            for(short j=0; table[i][j]!='\0'; ++j) table[i][j]=str[cnt++];
         }
     }
+}
 
-    cout<<"Where to output result: file or console\n";
-    char out;
-    cin>>out;
-    if(out=='f') freopen("res.txt","w",stdout);
-
-    cnt=0;i=0;j=0;
-    while(cnt++<=len){
-        if(i==0){
-            if(cnt==1) cout<<table[i][j++];
-            else{--cnt; ++j;}
-            if(++cnt<=len) cout<<table[i][j];
-        }
+void Decrypt(char (&table)[32][32],short& len){
+    short cnt=0,i=0,j=0;
+    cout<<table[i][j];
+    while(++cnt<=len){
+        if(i==0) cout<<table[i][++j];
         while(j>0 && ++cnt<=len) cout<<table[++i][--j];
         if(j==0 && ++cnt<=len) cout<<table[++i][j];
-        while(i!=0 && ++cnt<=len)  cout<<table[--i][++j];
+        while(i>0 && ++cnt<=len)  cout<<table[--i][++j];
     }
-    cout<<"\n";
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    SetConsoleCP(1251);SetConsoleOutputCP(1251);
+    const short R=32,C=32;
+    char table[R][C]={};
+
+    string str; getline(cin,str);
+    DeleteExtraSPACE(str);
+
+    short len=str.length();
+    BuildDoll(table,len);
+    FillDoll(table,len,str);
+
+    cout<<"Where to output result: file or console\n";
+    char out; cin>>out;
+    if(out=='f') freopen("res.txt","w",stdout);
+
+    Decrypt(table,len);
     return 0;
 }
