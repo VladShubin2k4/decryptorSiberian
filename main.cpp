fe6a14426f477b4e90808b79838590c5e56348d5
Vlad <VladShubin2k4@gmail.com>
2021-03-19 16:20:56 +0300

    Update main.cpp
    expansion of functionally

--- main.cpp
+++ main.cpp
@@ -26,11 +26,7 @@ void BuildDoll(char (&table)[32][32], short& len){
 
 void FillDoll(char (&table)[32][32], short& len, string& str){
     short cnt=0;
-    while(cnt<=len){
-        for(short i=0; cnt<=len; ++i){
-            for(short j=0; table[i][j]!='\0'; ++j) table[i][j]=str[cnt++];
-        }
-    }
+    while(cnt<=len) for(short i=0; cnt<=len; ++i) for(short j=0; table[i][j]!='\0'; ++j) table[i][j]=str[cnt++];
 }
 
 void Decrypt(char (&table)[32][32],short& len){
@@ -47,19 +43,21 @@ void Decrypt(char (&table)[32][32],short& len){
 int main(){
     ios::sync_with_stdio(false);
     SetConsoleCP(1251);SetConsoleOutputCP(1251);
-    const short R=32,C=32;
+    const short R=32,C=32;char choose;
     char table[R][C]={};
 
     string str; getline(cin,str);
-    DeleteExtraSPACE(str);
+    cout<<"Remove separating spaces? y/n?\n";
+    cin>>choose;
+    if(choose=='y') DeleteExtraSPACE(str);
 
-    short len=str.length();
+    short len=static_cast<short>(str.length());
     BuildDoll(table,len);
     FillDoll(table,len,str);
 
-    cout<<"Where to output result: file or console\n";
-    char out; cin>>out;
-    if(out=='f') freopen("res.txt","w",stdout);
+    cout<<"Where to output result: file (f) or console (c)?\n";
+    cin>>choose;
+    if(choose=='f') freopen("res.txt","w",stdout);
 
     Decrypt(table,len);
     return 0;
